c6aa0504210c11af303e689f0387242bceffb892
Junichi Ito <jit@sonicgarden.jp>
2014-04-28 07:49:55 +0900

    Refactoring.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,9 +1,5 @@
 class BonusDrink
   def self.total_count_for(amount)
-    sho, amari = amount.divmod(3)
-    returned_bottles = sho * 3
-    existing_bottles = sho + amari
-
-    returned_bottles + (existing_bottles > 2 ? total_count_for(existing_bottles) : existing_bottles)
+    amount.zero? ? 0 : amount + (amount - 1) / 2
   end
 end
