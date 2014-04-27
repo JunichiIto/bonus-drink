73011a0ec7ce02c967c3a156441e50b141747a91
Junichi Ito <jit@sonicgarden.jp>
2014-04-28 07:34:08 +0900

    Add answer.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,9 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    sho, amari = amount.divmod(3)
+    returned_bottles = sho * 3
+    existing_bottles = sho + amari
+
+    returned_bottles + (existing_bottles > 2 ? total_count_for(existing_bottles) : existing_bottles)
   end
 end
