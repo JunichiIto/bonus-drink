09adcffbfde316d860f50f413d7374dc59313dce
IzumiYoshiki <izumiyoshiki@icloud.com>
2016-04-06 14:50:43 +0900

    新規ブランチでテストコメント

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
-describe BonusDrink do
+describe BonusDrink do#test coment
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
 end
