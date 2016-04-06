97891cd35b47c98a8df6735ee9c4c085f6674e8f
IzumiYoshiki <izumiyoshiki@icloud.com>
2016-04-06 19:12:03 +0900

    問題の解答

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do#test coment
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it "買った本数" do
+    expect(BonusDrink.buy_count_for(3)).to eq 3
+  end
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
 end
