ee2d8daab021ea0e0092750530be211ace140d61
赤座 久樹 <the_red@Montblanc.local>
2016-09-06 11:02:46 +0900

    計算プログラムリファクタリング
    クラス変数@amountを廃止して、
    再帰呼び出しするサブルーチンを
    ボーナスの計算専用にした。

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,28 +1,23 @@
 class BonusDrink
   def self.total_count_for(amount)
-    @amount = amount
     @print_str = ""
-    self.count_for(amount)
+    amount += self.bonus_count_for(amount)
 
-    #@amount
-    @print_str += "総合計: #{@amount}\n"
+    #amount
+    @print_str += "総合計: #{amount}\n"
   end
 
-  def self.count_for(amount)
+  def self.bonus_count_for(amount)
     @print_str += "計算対象: #{amount}\n"
 
     bonus = amount.div(3)
     rem  = amount % 3
-    @amount += bonus
-
     @print_str += "#{amount}/3 -> #{bonus}*3 + #{rem} (#{bonus}本おまけ, #{rem}本余り)\n"
-    @print_str += "合計: #{@amount}\n"
 
     rem += bonus
     if rem >= 3
-      self.count_for(rem)
-    else
-      return bonus
+      bonus += self.bonus_count_for(rem)
     end
+    return bonus
   end
 end
