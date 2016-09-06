a6b2b4e0539ad7b35003473d230fb3d268abdb4e
赤座 久樹 <the_red@Montblanc.local>
2016-09-06 13:47:34 +0900

    計算プログラムリファクタリング
    returnを削除
    変数名を修正
    など

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -2,26 +2,26 @@ class BonusDrink
   BONUS_UNIT = 3
 
   def self.total_count_for(amount)
-    @print_str = "空き瓶#{BONUS_UNIT}本で1本おまけ\n"
+    @debug_print = "空き瓶#{BONUS_UNIT}本で1本おまけ\n"
     amount += self.bonus_count_for(amount)
 
-    #amount
-    @print_str += "総合計: #{amount}\n"
+    @debug_print += "総合計: #{amount}\n"
+    amount
   end
 
   def self.bonus_count_for(amount)
-    @print_str += "計算対象: #{amount}\n"
+    @debug_print += "計算対象: #{amount}\n"
 
     bonus = amount.div(BONUS_UNIT)
     rem  = amount % BONUS_UNIT
-    @print_str += "#{amount}/#{BONUS_UNIT} -> " \
-                + "#{bonus}*#{BONUS_UNIT} + #{rem} " \
-                + "(#{bonus}本おまけ, #{rem}本余り)\n"
+    @debug_print += "#{amount}/#{BONUS_UNIT} -> " \
+                 +  "#{bonus}*#{BONUS_UNIT} + #{rem} " \
+                 +  "(#{bonus}本おまけ, #{rem}本余り)\n"
 
     rem += bonus
     if rem >= BONUS_UNIT
       bonus += self.bonus_count_for(rem)
     end
-    return bonus
+    bonus
   end
 end
