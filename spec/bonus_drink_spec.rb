76b9cf5e82d7a9a987aee549de59058aa28a1353
ikegamikanae <fujii_kanae@hotmail.com>
2018-05-24 23:45:36 +0900

    add case amount=100

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -5,4 +5,5 @@ describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
   specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
   specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
 end
