29c8fa64581b94684e8431610450c45adb8f4711
ikegamikanae <fujii_kanae@hotmail.com>
2018-05-24 15:00:41 +0900

    1st draft

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,19 @@
 class BonusDrink
+  @@bonus_bin = 3
   def self.total_count_for(amount)
-    0
+    buy_bottle = amount
+    emp_bottle = amount       # 未返却の空き瓶
+    get_bottle = 0            # ボーナスとしてもらったボトル
+    total_bottle = amount
+
+    # while do
+    while emp_bottle < @@bonus_bin
+      get_bottle = emp_bottle.div(@@bonus_bin)
+      total_bottle += get_bottle
+      emp_bottle = emp_bottle - (get_bottle * @@bonus_bin) + get_bottle
+    end
+    puts("トータルで飲んだ本数は#{get_bottle}です。")
   end
 end
+
+BonusDrink.total_count_for(100)
