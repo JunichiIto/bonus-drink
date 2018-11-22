17482dd4c1a45afc5d0eeea23b506c8820a1055e
donaisore <r.h.kobe0706@gmai.com>
2018-11-22 12:33:46 +0900

    first answer commit

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total_count = amount
+    rest_bottles = amount
+    while new_bottles(rest_bottles) > 0 do
+      new_bottles = new_bottles(rest_bottles)
+      rest_bottles = new_bottles + rest_bottles % 3
+      total_count += new_bottles
+    end
+    return total_count
   end
+
+  def self.new_bottles(amount)
+    return amount / 3
+  end
+
 end
