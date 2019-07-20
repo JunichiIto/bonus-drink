42455479f8d228437e366af67c9bf9e20116770c
HisAisKey <logical0egoistic0pip@gmail.com>
2019-07-21 00:30:52 +0900

    bonus_drink with rspec _comp

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+      specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+      specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+      specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
 end
