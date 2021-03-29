9ca995e8942b1db620c03643f2ce0aa0e7f913bb
Riki2216 <81208877+Riki2216@users.noreply.github.com>
2021-03-29 13:23:36 +0900

    Answer bonus drink -Tagawa
    回答を作成しました。理解が進まず、時間がかかりました。よろしくお願いします。

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    amount + amount.pred.abs / 2
+    drink = ->(left, drinked){
+            return drinked if left.zero?
+            i = left / 3
+            d = i.zero? ? left : i * 3
+            drink.(left - d + i, drinked + d)
+        }
+        drink.(amount, 0)
   end
 end
